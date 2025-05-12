db98d0f1fe24395376a132d773a552168361d3b3
MHuzaifaHussain <muhammadh.and.dev@gmail.com>
2025-05-12 10:27:04 -0700

    Add Video Player App

---
+++ VideoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.videoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView = findViewById(R.id.videoView);
+
+        String vPath= "android.resource://"+getPackageName()+"/raw/video";
+        String oPath = "online video link";
+        Uri videoUri = Uri.parse(vPath);
+
+//        videoView.setVideoPath(vPath);
+        videoView.setVideoURI(videoUri);
+        videoView.start();
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+    }
+}
